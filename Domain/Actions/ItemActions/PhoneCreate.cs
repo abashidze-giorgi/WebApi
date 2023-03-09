aa56e8d5689cc19510aa0be8d6820a64c953a0dd
giorgi abashidze <gabashidze99@gmail.com>
2023-03-09 23:17:15 +0400

    finish PhoneCreate class methods

--- Domain/Actions/ItemActions/PhoneCreate.cs
+++ Domain/Actions/ItemActions/PhoneCreate.cs
@@ -10,24 +10,94 @@ namespace Domain.Actions.ItemActions
 {
     public class PhoneCreate : IItemActions<List<TelephoneModel>, TelephoneModel, TelephoneModel>
     {
-        public bool CheckItemExist(List<TelephoneModel> itemList, TelephoneModel item, out string result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool CheckItemWhenEdit(List<TelephoneModel> itemList, TelephoneModel item, out string result)
-        {
-            throw new NotImplementedException();
-        }
 
         public TelephoneModel Create(List<TelephoneModel> itemList, TelephoneModel item, out string result)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var createdItem = new TelephoneModel
+                {
+                    Name = item.Name,
+                    Battery = item.Battery,
+                    Display = item.Display,
+                    Model = item.Model,
+                    Price = item.Price,
+                    Rating = item.Rating,
+                };
+                if (!CheckItemExist(itemList, createdItem, out result))
+                {
+                    result = "Ok";
+                    return createdItem;
+                }
+                result = "Product exist";
+                return item;
+            }
+            catch (Exception ex)
+            {
+                result = ex.Message;
+                return item;
+            }
         }
 
         public TelephoneModel Update(List<TelephoneModel> itemList, TelephoneModel item, out string result)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var newItem = new TelephoneModel
+                {
+                    Name = item.Name,
+                    Battery = item.Battery,
+                    Display = item.Display,
+                    Model = item.Model,
+                    Price = item.Price,
+                    Rating = item.Rating,
+                };
+                if (CheckItemWhenEdit(itemList, item, out result))
+                {
+                    newItem.Id = item.Id;
+                    return newItem;
+                }
+                else
+                {
+                    return item;
+                }
+            }
+            catch (Exception ex)
+            {
+                result = ex.Message;
+                return item;
+            }
         }
+
+        #region Private Methods
+
+        public bool CheckItemExist(List<TelephoneModel> itemList, TelephoneModel item, out string result)
+        {
+            foreach (var u in itemList)
+            {
+                if (u.Name == item.Name && u.Model == item.Model)
+                {
+                    result = "Ok";
+                    return true;
+                }
+            }
+            result = "Can't find product";
+            return false;
+        }
+
+        public bool CheckItemWhenEdit(List<TelephoneModel> itemList, TelephoneModel item, out string result)
+        {
+            foreach (var it in itemList)
+            {
+                if (it.Id == item.Id)
+                {
+                    result = "Ok";
+                    return true;
+                }
+            }
+            result = "Can't find user";
+            return false;
+        }
+        #endregion
     }
 }
